8c942385dbb755b5790d9d558bfc915d5bd8092a
Yuxuan <yuxuan_zhang13@outlook.com>
2024-05-08 17:54:02 -0700

    update deployment

---
+++ todomvc/build/_app/immutable/entry/start.C0qpaXwA.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BHugRe3s.js";export{t as start};
--- todomvc/src/routes/+page.svelte
+++ todomvc/src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
+    import { base } from '$app/paths';
     import App from '../components/App.svelte';
-</script>
-
-<App />
+  </script>
+  
+  <nav>
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+  </nav>
+  
+  <App />
